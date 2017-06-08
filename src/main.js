1223aa3ef1ac6918332b3ef78f545972ae3467e7
nokelong <nokelong@gmail.com>
2017-06-08 17:18:06 +0800

    搭建项目基本结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'es6-shim'
+import css from '@/assets/css/main.css'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
---
+++ src/views/index.vue
@@ -0,0 +1,27 @@
+<template>
+ 	<header class="d-box headBar p_fixed bg_blue HomeheadBar">
+		<aside class="pl_15 ta_avC b-flex">
+			<a class="btnBox headBarLogo" href="javascript:;">
+				<span class="fz_16 c_fff">
+					<img src="../assets/images/logo.png" alt="">
+				</span>
+			</a>
+		</aside>
+		<aside class="pr_15 ta_avC">
+			<a class="btnBox" href="javascript:;">
+				<i class="iconfont wf-bookshelf fz_18"></i>
+			</a>
+			<a class="btnBox" href="javascript:;">
+				<i class="iconfont wf-search fz_18"></i>
+			</a>
+			<a class="btnBox" href="javascript:;">
+				<i class="icon i-user hide mt_10"></i><!-- 未登录状态 -->
+				<i class="icon i-userSign  mt_10"></i><!-- 已登录状态 -->
+			</a>
+		</aside>
+	</header>
+</template>
+<style>
+</style>
+<script>
+</script>
